cb55945a4c3f05688d9923c8c0d9b2b0175bf51c
Dhruv Arora <98750226+lord-benjamin@users.noreply.github.com>
2023-03-25 04:32:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const domToImage = require("dom-to-image");
+const ejs = require("ejs");
 
 const app = express();
 const port = process.env.PORT || 3000;
