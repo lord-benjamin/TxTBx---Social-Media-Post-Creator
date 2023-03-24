6ad342eefe4896c2771437242e5d0eb4ee2f7bb6
Dhruv Arora <98750226+lord-benjamin@users.noreply.github.com>
2023-03-25 04:38:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.use(express.static(__dirname + '/views'));
 app.set("view engine", "ejs");
 
 const socials = [
